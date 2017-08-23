5b0054dadedc0822c237b5fd64740933eaafda9b
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-23 19:37:13 +0200

    Import and use vuetify

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-
+import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
 import store from './store'
 
+Vue.use(Vuetify)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
