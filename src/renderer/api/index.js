40c2307144eddb718d7ee6455ea27a9cc299db1c
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-23 23:32:58 +0200

    remove unised code in the api

--- src/renderer/api/index.js
+++ src/renderer/api/index.js
@@ -1,19 +1,8 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const { fsExistsSync } = require('./fs')
-const fs = require('fs')
-const pathToDb = require('os').homedir() + '/.db'
-
-if (!fsExistsSync(pathToDb, fs)) {
-  try {
-    fs.writeFileSync(pathToDb, null)
-  } catch (e) {
-    console.log('could not create file')
-  }
-}
-
 const adapter = new FileSync('db.json')
 const db = low(adapter)
+
 /* Set some defaults if your JSON file is empty
   the expected shape of each task is like so
   {
@@ -38,6 +27,7 @@ function createTask (task) {
 function removeTask (id) {
   db.get('tasks')
     .remove({ id })
+    .cloneDeep()
     .write()
 }
 
