fcc79939709394291a1b94e905a68298595efb5b
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-23 19:43:33 +0200

    Show alerts if need be in the App.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
+    <!-- show alerts using the snackbar -->
+    <v-snackbar
+      :success="alert.type === 'success'"
+      :warning="alert.type === 'warning'"
+      :info="alert.type === 'info'"
+      :error="alert.type === 'error'"
+      :timeout="2000"
+      top
+      v-model="alert.show"
+    >
+      {{ alert.title }}
+      <v-btn class="white" @click.native="showAlert = false"><v-icon>close</v-icon>Close</v-btn>
+    </v-snackbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'assignme'
+    name: 'assignme',
+    mounted () { console.log(this.$store.getters.alert) },
+    data () { return { showAlert: this.alert.show } },
+    watch: {
+      showAlert (value) {
+        if (value === false && this.alert.show === true) {
+          this.$store.commit('closeAlert')
+        }
+      }
+    },
+    computed: {
+      alert () {
+        return this.$store.getters.alert
+      }
+    }
   }
 </script>
 
-<style>
+<style lang="stylus">
   /* CSS */
+   @import './stylus/main'
 </style>
