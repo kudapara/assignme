b6b138720dcb98de8cd33eb48beb945a9313c2b7
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-24 00:13:37 +0200

    Playing around with css

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -38,5 +38,18 @@
 
 <style lang="stylus">
   /* CSS */
-   @import './stylus/main'
+  @import './stylus/main'
+  body { font-family: 'Ubuntu', 'Source Sans Pro', sans-serif; }
+
+  #app {
+    background:
+      radial-gradient(  
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
 </style>
