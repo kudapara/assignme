ff61bae6c24baba00cf86ff39892183cd80a3075
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-24 01:08:28 +0200

    Add a button for updates

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -16,6 +16,7 @@
     <v-toolbar class="elevation-0" dense>
       <img id="logo" src="~@/assets/logo.png" alt="electron-vue" style="height: 50px">
       <v-spacer></v-spacer>
+      <v-btn class="pink white--text">Updates</v-btn>
       <v-chip outline label class="pink pink--text">
         v{{version}}
       </v-chip>
