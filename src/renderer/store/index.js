1f4288561c0c986783b50cf6d8a4495183bbb83e
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-23 19:39:58 +0200

    Add vuex state and getters to access the api

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -2,10 +2,57 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import modules from './modules'
+import api from '../api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  state: {
+    tasks: [],
+    alert: {
+      title: '',
+      type: 'info', // Either error, info or warning
+      show: false
+    }
+  },
+  mutations: {
+    addTask (state, payload) {
+      payload.id = Math.random()
+      payload.created = new Date()
+      api.createTask(payload)
+      state.alert = {
+        title: 'successfully created task',
+        type: 'info',
+        show: true
+      }
+    },
+
+    getTasks (state) {
+      state.tasks = api.getTasks()
+    },
+
+    removeTask (state, task) {
+      api.removeTask(task.id)
+      state.tasks.splice(state.tasks.findIndex(x => x.id === task.id), 1)
+      state.alert = {
+        title: 'successfully removed task',
+        type: 'info',
+        show: true
+      }
+    },
+
+    closeAlert (state) {
+      state.alert = state.alert = {
+        title: '',
+        type: '',
+        show: false
+      }
+    }
+  },
+
+  getters: {
+    alert: (state) => state.alert
+  },
   modules,
   strict: process.env.NODE_ENV !== 'production'
 })
