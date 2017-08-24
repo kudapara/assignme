8714bd47bb2041899d0f02f853544e4e41b8d35d
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-24 12:26:11 +0200

    Make A seperate component AppToolbar for the toolbar

---
+++ src/renderer/components/AppToolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-toolbar class="elevation-0" dense>
+    <img id="logo" src="~@/assets/logo.png" alt="electron-vue" style="height: 50px">
+    <v-spacer></v-spacer>
+    <v-btn class="pink white--text" @click.native="open('https://github.com/kudapara/assignme/releases')">Updates</v-btn>
+    <v-chip outline label class="pink pink--text">
+      v{{version}}
+    </v-chip>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        version: require('../../../package.json').version
+      }
+    },
+
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
