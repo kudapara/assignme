cce8d85e42028ffd2d0ddce196d894250637cced
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-24 20:03:52 +0200

    [AppAlert.vue] Add a show data property based off of the showAlert prop
    -- This is to prevent v-model from directly mutating the state

--- src/renderer/components/AppAlert.vue
+++ src/renderer/components/AppAlert.vue
@@ -6,7 +6,7 @@
       :error="alert.type === 'error'"
       :timeout="2000"
       top
-      v-model="alert.show">
+      v-model="show">
       {{ alert.title }}
       <v-btn class="white" @click.native="$emit('close')">
         <v-icon>close</v-icon> Close
@@ -17,8 +17,22 @@
 <script>
   export default {
     name: 'AppAlert',
+    
+    data () {
+      return {
+        show: this.showAlert
+      }
+    },
+
+    watch: {
+      showAlert (value) {
+        this.show = value
+      }
+    },
+
     props: {
-      alert: Object
+      alert: Object,
+      showAlert: Boolean
     }
   }
 </script>
