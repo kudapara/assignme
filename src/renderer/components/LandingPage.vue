ea3136a2d66cde3f5a6721aa451e1f94d7983a15
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-27 00:09:05 +0200

    bump version to 0.15.0

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="wrapper">
-    <v-toolbar class="pink mb-2 elevation-1">
-      <v-toolbar-title class="white--text">Assignment Tasks ({{tasks.length}})</v-toolbar-title>
+    <v-toolbar light class="pink mb-2 elevation-1">
+      <v-btn-toggle class="white elevation-4" v-bind:items="taskStatuses" mandatory v-model="taskStatus"> </v-btn-toggle>
       <v-spacer></v-spacer>
       <v-btn to="/create"><v-icon class="pink--text">add</v-icon> New task</v-btn>
       <v-btn-toggle class="white" v-bind:items="displayOptions" mandatory v-model="display"> </v-btn-toggle>
     </v-toolbar>
 
+    <!-- TASK TIMELINE COMPONENT -->
     <AppTaskTimeline
-      :tasks="tasks"
+      :tasks="displayTasks"
       @deleteTask="showRemoveDialog"
       @startTask="startTask"
       @editTask="editTask"
@@ -17,8 +18,9 @@
       v-if="showTimelineComponent">
     </AppTaskTimeline>
 
+    <!-- TASK LIST COMPONENT -->
     <AppTaskList
-      :tasks="tasks"
+      :tasks="displayTasks"
       @deleteTask="showRemoveDialog"
       @startTask="startTask"
       @editTask="editTask"
@@ -27,7 +29,7 @@
       v-if="showListComponent">
     </AppTaskList>
 
-    <v-card v-if="!tasks.length">
+    <v-card v-if="!allTasks.length">
       <v-card-text>
         <h3 class="headline">You have no tasks.</h3>
         <p>In the mean time you could use your time to research, socialise or dream big.</p>
@@ -70,24 +72,45 @@
         displayOptions: [
           { icon: 'timeline', value: 'timeline' },
           { icon: 'list', value: 'list' }
-        ]
+        ],
+        taskStatus: 'all'
       }
     },
 
     mounted () { this.$store.commit('getTasks') },
 
     computed: {
-      tasks () {
+      displayTasks () {
+        return this[`${this.taskStatus}Tasks`]
+      },
+      allTasks () {
         return this.$store.getters.tasks
+      },
+      idleTasks () {
+        return this.allTasks.filter(task => task.status === 'new')
+      },
+      startedTasks () {
+        return this.allTasks.filter(task => task.status === 'in_progress')
+      },
+      finishedTasks () {
+        return this.allTasks.filter(task => task.status === 'done')
       },
       alert () {
         return this.$store.getters.alert
       },
       showTimelineComponent () {
-        return this.display === 'timeline' && this.tasks.length > 0
+        return this.display === 'timeline' && this.allTasks.length > 0
       },
       showListComponent () {
-        return this.display === 'list' && this.tasks.length > 0
+        return this.display === 'list' && this.allTasks.length > 0
+      },
+      taskStatuses () {
+        return [
+          { text: `all (${this.allTasks.length})`, value: 'all' },
+          { text: `idle (${this.idleTasks.length})`, value: 'idle' },
+          { text: `started (${this.startedTasks.length})`, value: 'started' },
+          { text: `finished (${this.finishedTasks.length})`, value: 'finished' }
+        ]
       }
     },
 
