4c468ad35f114cae78b6e8bc1ed654afd571fa25
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-09-10 11:22:45 +0200

    [App] Implement the AppNAvigationDrawer in the app
    -- Also add a footer showing the version
        And only show toolbar only iff the user is authenticated

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -2,32 +2,47 @@
   <v-app id="app">
     <!-- show alerts using the snackbar -->
     <app-alert :alert="alert" :showAlert="showAlert" @close="closeAlert"></app-alert>
-    <app-toolbar></app-toolbar>
+    <app-navigation-drawer v-if="authenticated"></app-navigation-drawer>
+    <app-toolbar v-if="authenticated"></app-toolbar>
 
-    <v-container id="wrapping">
-      <router-view></router-view>
-    </v-container>
+    <!-- The Main content -->
+    <main>
+      <v-container id="wrapping" fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
+
+    <!--The footer region -->
+    <v-footer class="pa-3" fixed>
+      <v-spacer></v-spacer>
+      <div class="pink--text">assignme v{{version}}</div>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
   import AppAlert from '@/components/AppAlert'
+  import AppNavigationDrawer from '@/components/AppNavigationDrawer'
   import AppToolbar from '@/components/AppToolbar'
 
   export default {
     name: 'assignme',
 
-    components: { AppAlert, AppToolbar },
+    components: { AppAlert, AppNavigationDrawer, AppToolbar },
 
     data () {
       return {
-        showAlert: false
+        showAlert: false,
+        version: require('../../package.json').version
       }
     },
 
     methods: {
       closeAlert () {
         this.$store.commit('closeAlert')
+      },
+      open (link) {
+        this.$electron.shell.openExternal(link)
       }
     },
 
@@ -35,7 +50,8 @@
       alert () {
         this.showAlert = this.$store.getters.alert.show
         return this.$store.getters.alert
-      }
+      },
+      authenticated () { return !!this.$store.getters.authUser === true }
     }
   }
 </script>
