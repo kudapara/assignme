8b4439621127fcbfcff29ffebfdd0784973b0241
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-09-10 11:39:58 +0200

    [AppToolBar] Remove the check fot updates and sign out button
    -- * the version is now being shown in the footer
       * the updates and signout are now actions in the AppNavigationDrawer

--- src/renderer/components/AppToolbar.vue
+++ src/renderer/components/AppToolbar.vue
@@ -1,57 +1,30 @@
 <template>
-  <v-toolbar light class="elevation-1" dense>
-    <v-btn flat @click.native="goBack" v-if="showBackBtn">
+  <v-toolbar light class="elevation-1" dense v-if="showToolbar">
+    <!-- back button.-->
+    <v-btn flat @click.native="goBack">
       <v-icon>chevron_left</v-icon> Back
     </v-btn>
-    <v-toolbar-title class="pink--text">assignme</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <!--sho user the version and allow them to check for updates-->
-    <v-menu open-on-click offset-y>
-      <v-btn outline dark slot="activator" class="pink pink--text"><v-icon class="pink--text">label</v-icon> {{version}}</v-btn>
-      <v-list>
-          <v-btn flat class="pink--text" @click.native="open('https://github.com/kudapara/assignme/releases')"><v-icon class="pink--text">update</v-icon> Check for Updates</v-btn>
-      </v-list>
-    </v-menu>
 
+    <v-spacer></v-spacer>
+    
+    <!-- The sign in button -->
     <v-btn
       to="/signin"
       v-if="showSignInBtn"
       class="pink white--text">
       Sign in
     </v-btn>
-
-    <v-btn
-      v-if="showSignOutBtn"
-      @click.native="signOut"
-      class="pink white--text">
-      <v-icon class="white--text">lock_outline</v-icon> Lock App
-    </v-btn>
   </v-toolbar>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        version: require('../../../package.json').version
-      }
-    },
-
     methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      },
-
       goBack () {
         // whenever you leave the create task window, cleat the taskToEdit
         // so that when a new task is going to be created there will be no problem
         this.$store.commit('setTaskToUpdate', null)
         this.$router.push('/')
-      },
-
-      signOut () {
-        this.$store.commit('signOut')
-        this.$router.push('/signin')
       }
     },
 
@@ -60,16 +33,12 @@
         return this.$store.getters.authUser
       },
 
-      showBackBtn () {
-        return this.$route.name !== 'landing-page' && this.$route.name !== 'signin-page'
+      showToolbar () {
+        return this.$route.name === 'create-task'
       },
 
       showSignInBtn () {
         return !this.authUser && this.$route.name !== 'signin-page' && this.$route.name !== 'signup-page'
-      },
-
-      showSignOutBtn () {
-        return this.authUser
       }
     }
   }
