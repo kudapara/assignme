6dbddf70b3d6c4756db40c94ad37860c665cfecf
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-10 19:43:05 +0200

    Split the timeline into multiple files
     --Also add the delete dialog as a seperate component
     --use the mixin also

---
+++ src/renderer/components/AppConfirmDeleteTask.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- Ask for user to confirm delete action -->
+    <v-layout row justify-center style="position: relative;">
+      <v-dialog v-model="show" persistent>
+        <v-card dark>
+          <v-card-title>
+            <div class="headline">Delete selected task</div>
+          </v-card-title>
+          <v-card-text>Are you sure you want to delete the <b>{{taskToDelete.title}}</b> task?</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="green" @click.native="$emit('close')">Don't delete</v-btn>
+            <v-btn class="red--text darken-1" flat="flat" @click.native="removeTask">Go ahead</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppConfirmDeleteTask',
+    props: {
+      taskToDelete: Object,
+      show: Boolean
+    },
+    methods: {
+      removeTask () {
+        this.$store.dispatch('removeTask', this.taskToDelete)
+        this.$emit('close')
+      }
+    }
+  }
+</script>
---
+++ src/renderer/components/AppTaskTimeline/index.vue
@@ -0,0 +1,71 @@
+<template>
+ <div>
+   <!-- Show dialog adking user to confirm if he/she really wants to delete the task -->
+   <AppConfirmDeleteTask
+    :show="showDialog"
+    :taskToDelete="taskToDelete"
+    @close="showDialog = false; taskToDelete = {};">
+    </AppConfirmDeleteTask>
+
+    <section id="cd-timeline" class="cd-container" v-if="displayTasks.length">
+      <div class="cd-timeline-block" v-for="task in displayTasks" :key="task.id" >
+        <div :class="{
+        new: task.status === 'new',
+        in_progress: task.status === 'in_progress',
+        done: task.status === 'done',
+        task: true,
+        'cd-timeline-img': true}">
+        </div>
+
+        <div class="cd-timeline-content">
+          <h3 class="title">{{task.title}}
+            <v-chip  outline :class="{
+                red: task.status === 'new',
+                'red--text':task.status === 'new',
+                orange: task.status === 'in_progress',
+                'orange--text':task.status === 'in_progress',
+                green: task.status === 'done',
+                'green--text':task.status === 'done'
+              }">
+              {{task.status}}
+            </v-chip>
+          </h3>
+          <p>{{task.description}}</p>
+
+          <v-btn small fab class="cd-read-more  white--text" @click.native="taskToDelete = task; showDialog = true;">
+            <v-icon class="red--text">delete</v-icon>
+          </v-btn>
+          <v-btn small fab class="cd-read-more white--text" @click.native="editTask(task)">
+            <v-icon class="green--text">edit</v-icon>
+          </v-btn>
+         
+          <v-btn v-if="task.status === 'new'" class="cd-read-more orange white--text" @click.native="startTask(task)">Start task</v-btn>
+          <v-btn v-if="task.status === 'in_progress'" class="cd-read-more green white--text" @click.native="finishTask(task)">Finish task</v-btn>
+          <v-btn v-if="task.status === 'done'" class="cd-read-more orange white--text" @click.native="startTask(task)">Restart task</v-btn>
+          <span class="cd-date"><b>Due {{task.deadline | niceDate}}</b></span>
+        </div>
+      </div>
+  
+  </section>
+ </div>
+</template>
+
+<script>
+  import taskMixin from '@/components/mixins/tasks'
+  import AppConfirmDeleteTask from '@/components/AppConfirmDeleteTask'
+
+  export default {
+    name: 'AppTaskTimeline',
+    data () {
+      return {
+        showDialog: false
+      }
+    },
+    mixins: [taskMixin],
+    components: { AppConfirmDeleteTask }
+  }
+</script>
+
+<style scoped>
+  @import './timeline.css'
+</style>
